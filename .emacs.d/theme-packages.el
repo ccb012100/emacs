059e3694bc4a5fa9861312d3d75e498b3836278f
Christopher Bocardo <ccb012100@users.noreply.github.com>
2019-10-16 06:45:44 -0500

    removed zenburn; added atom-one-dark-theme

--- .emacs.d/theme-packages.el
+++ .emacs.d/theme-packages.el
@@ -2,9 +2,9 @@
 ;;;;
 ;;;; located in ~/.emacs.d/theme-packages.el
 
+(use-package atom-one-dark-theme)
 (use-package color-theme-sanityinc-tomorrow)
 (use-package darktooth-theme)
 (use-package dracula-theme)
 (use-package gruvbox-theme)
 (use-package monokai-theme)
-(use-package zenburn-theme)
