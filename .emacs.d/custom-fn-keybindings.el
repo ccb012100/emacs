56ddc217070128182bff62dec4d3b9e9b9545821
Christopher Bocardo <ccb012100@users.noreply.github.com>
2019-10-16 07:14:05 -0500

    reorganized; added keybinding for move-to-window-line-top-bottom

--- .emacs.d/custom-fn-keybindings.el
+++ .emacs.d/custom-fn-keybindings.el
@@ -1,13 +1,19 @@
 ;;;; Keybindings bound to custom functions defined in ~/.emacs./defuns.el
-;;;;
-;;;; located in ~/.emacs.d/custom-fn-keybindings.el
 
+;;; Meta
 (global-set-key (kbd "M-<return>") #'my/new-line-below)
+(global-set-key (kbd "M-T") #'my/reopen-killed-file)
 
-(global-set-key (kbd "M-T") #'my/reopen-killed-file) ; <SHIFT> + <META> + t
+;;; Super
+(global-set-key (kbd "s-k") #'my/save-and-kill-buffer)
+(global-set-key (kbd "s-r") #'move-to-window-line-top-bottom)
+
+;;; Ctrl-Meta
 (global-set-key (kbd "C-M-<return>") #'my/open-new-line-above)
+
 (global-set-key (kbd "C-<return>") #'my/open-new-line-below)
 
+;;; Ctrl
 (global-set-key (kbd "C-j") #'my/join-lines)
 (global-set-key (kbd "C-o") #'my/move-beginning-of-line-enhanced)
 (global-set-key (kbd "C-w") #'my/mark-word-under-cursor)
@@ -19,7 +25,6 @@
 (global-set-key (kbd "C-l k") #'my/duplicate-line-or-region)
 
 (global-set-key (kbd "C-x k") #'my/save-and-kill-buffer)
-(global-set-key (kbd "s-k") #'my/save-and-kill-buffer)
 
 (add-hook #'emacs-lisp-mode-hook
           (lambda ()
