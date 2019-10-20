9f437bf5bb4e43291e15e38819f1aa1e13e5860f
Christopher Bocardo <ccb012100@users.noreply.github.com>
2019-10-20 15:02:48 -0500

    added themes

--- .emacs.d/packages/theme-packages.el
+++ .emacs.d/packages/theme-packages.el
@@ -4,10 +4,13 @@
 
 (use-package atom-one-dark-theme)
 (use-package color-theme-sanityinc-tomorrow)
+(use-package cyberpunk-theme)
 (use-package darktooth-theme)
 (use-package dracula-theme)
 (use-package gruvbox-theme)
 (use-package monokai-theme)
 (use-package monokai-alt-theme)
 (use-package monokai-pro-theme)
+(use-package nord-theme)
 (use-package solarized-theme)
+(use-package zerodark-theme)
