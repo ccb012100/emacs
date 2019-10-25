3b3b181a5f63fdcadda832b0d01a955a2e807b33
Christopher Bocardo <ccb012100@users.noreply.github.com>
2019-10-25 06:45:23 -0500

    added show-in-finder fn; file cleanup

--- .emacs.d/macOS.el
+++ .emacs.d/macOS.el
@@ -1,16 +1,24 @@
 ;;;; MacOS-specific settings
 ;;;; located at ~/.emacs.d/macOS.el
 (when (eq system-type #'darwin)
-  (setq mac-option-modifier #'super) ; 'alt/option' sends 'M'
-  (setq mac-command-modifier #'meta) ; 'command' sends 'M'
+  (setq mac-option-modifier #'super)         ; 'alt/option' sends 'M'
+  (setq mac-command-modifier #'meta)         ; 'command' sends 'M'
+
   (global-set-key [kp-delete] #'delete-char) ; maps fn-delete to right-delete
-  (set-face-attribute #'default nil :family "Hack" :height 120) ; set font
+
+  (defun mac/show-in-finder ()
+    "Show current buffer file in macOS Finder."
+    (interactive)
+    (shell-command "open ."))
+
+  ;; set font properties
+  (set-face-attribute #'default nil :family "Hack" :height 120)
 
   ;; ITERM2 MOUSE SUPPORT
   (unless window-system
     (require #'mouse)
     (xterm-mouse-mode t)
-    (defun track-mouse (e)) 
+    (defun track-mouse (e))
     (setq mouse-sel-mode t))
 
   ;; add `/usr/local/bin` to PATH and exec-path
